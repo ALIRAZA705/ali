d7f4dd9c76d2a34b19921474304960176a0d1407
ALIRAZA705 <aliraza30028@gmail.com>
2019-12-19 19:37:32 +0500

    =================

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     //hiiiiiii
     //aoa
     //aoaooaoaoaoaoa
+    //=====================
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
